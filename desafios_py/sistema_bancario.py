28ec8970b1ef251c8eb345a41309b1c9bfb427c1
Eveli-lima <eveliazevedo70@gmail.com>
2025-07-15 17:11:01 -0300

    refactor: utilizando funções no sistema bancário

--- desafios_py/sistema_bancario.py
+++ desafios_py/sistema_bancario.py
@@ -1,4 +1,11 @@
-﻿menu = f'''
+﻿
+saldo = 0
+limite = 500
+saques = 0
+LIMITE_SAQUE = 3
+historico = ""
+
+menu = f'''
     ============= Bem Vindo! ==============
     
     [1] DEPÓSITO
@@ -9,11 +16,46 @@
    ======================================== 
 '''
 
-saldo = 0
-limite = 500
-saques = 0
-LIMITE_SAQUE = 3
-extrato = ""
+def deposito(saldo, historico):
+    entrada = float(input("Digite o valor do Depósito: ").replace(',', '.'))
+    if entrada > 0:
+        saldo += entrada
+        historico += f"Depósito: R$ {entrada:.2f}\n"
+    else:
+        print("Falha na operação! O valor deve ser maior que zero.")
+    return saldo, historico
+
+def saque(saldo, limite, saques, LIMITE_SAQUE, historico):
+    saida = float(input("Digite o valor do saque: ").replace(',', '.'))
+
+    excedeu_saldo = saida > saldo
+    excedeu_limite = saida > limite
+    excedeu_saques = saques >= LIMITE_SAQUE
+
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+
+    elif saida > 0:
+        saldo -= saida
+        historico += f"Saque: R$ {saida:.2f}\n"
+        saques += 1
+
+    else:
+        print("Falha na operação! O valor é inválido")    
+
+    return saldo, saques, historico
+
+def extrato(saldo, historico):
+    print("extrato".center(20, "=")) 
+    print("Não foram realizadas movimentações." if not historico else historico)
+    print(f"\nSaldo: R$ {saldo:.2f}") 
+    print("=".center(20, "="))   
 
  
 while True:
@@ -23,45 +65,16 @@ while True:
 
         # DEPÓSITO
         if opcao == 1:
-            entrada = float(input("Digite o valor do Depósito: ").replace(',', '.'))
-
-            if entrada > 0:
-                saldo += entrada
-                extrato += f"Depósito: R$ {entrada:.2f}\n"
-            else:
-                print("Falha na operação! O valor deve ser maior que zero.")
+            saldo, historico = deposito(saldo, historico)
 
         # SAQUE
         elif opcao == 2:
-            saida = float(input("Digite o valor do saque: ").replace(',', '.'))
-
-            excedeu_saldo = saida > saldo
-            excedeu_limite = saida > limite
-            excedeu_saques = saques >= LIMITE_SAQUE
-
-            if excedeu_saldo:
-                print("Operação falhou! VocÊ não tem saldo suficiente.")
-
-            elif excedeu_limite:
-                print("Operação falhou! O valor do saque excede o limite.")
-
-            elif excedeu_saques:
-                print("Operação falhou! Número máximo de saques excedido.")
-
-            elif saida > 0:
-                saldo -= saida
-                extrato += f"Saque: R$ {saida:.2f}\n"
-                saques += 1
-
-            else:
-                print("Falha na operação! O valor é inválido")    
+            saldo, saques, historico = saque(saldo, limite, saques, LIMITE_SAQUE, historico)
         
         # EXTRATO
         elif opcao == 3:
-            print("extrato".center(20, "=")) 
-            print("Não foram realizadas movimentações." if not extrato else extrato)
-            print(f"\nSaldo: R$ {saldo:.2f}") 
-            print("=".center(20, "="))   
+            extrato(saldo, historico)
+           
 
         #SAIR
         elif opcao == 0:
@@ -69,13 +82,13 @@ while True:
             msn = f''' 
     ========= Obrigada, Volte sempre! =========
 
-    Sistema Bancário v1.0 
+    Sistema Bancário v1.1 
     Desenvolvido por: Éveli Lima 
                 '''
             print(msn)
             break
         else:
-            print("O valor digitado não é válido. \nDigite um número de 0 a 3.")
+            print("O valor digitado não é válido. \nDigite um número de 1 a 3. \nOu 0 para sair.")
     
     except ValueError:
         print("Falha na operação! O valor digitado não é um número válido. \nDigite um número de 0 a 3.")   
